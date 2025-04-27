67990de75a086e2b92b51a39333459cc99c00e61
LakhmiDeepak27 <lakshmideepak.mpc1836@gmail.com>
2025-04-27 19:46:59 +0530

    updates

--- konnectia/authentication/views.py
+++ konnectia/authentication/views.py
@@ -1,11 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 # Create your views here.
 
 def homePage(request):
     return render(request , "homePage.html")
 
 def signup(request):
+
+    if request.method == "POST":
+        username = request.POST['username']
+        first_name = request.POST['fname']
+        last_name = request.POST['lname']
+        email = request.POST['email']
+        password = request.POST['pass1']
+        cpassword = request.POST['pass2']
+
+        myuser= User.objects.create_user(username,password,email) 
+        myuser.first_name = first_name
+        myuser.last_name = last_name
+        myuser.save()
+
+        messages.success(request , "Your account has been created successfully")
+        return redirect('signin')
     return render(request , "authentication/signup.html")
 
 def signin(request):
